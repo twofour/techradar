1d3459c34d43c4f6641c3f9a20216e720daaa91b
Bennet KlarhÃ¶lter <b.klarhoelter@eraffe-media.de>
2017-05-31 17:26:39 +0200

    Refactoring

--- radar.js
+++ radar.js
@@ -49,21 +49,22 @@ function Radar() {
     };
 
     this.drawBackground = function () {
-        var space = (this.size / 4);
+        var space = (this.size / 4),
+            bgColor = 'rgba(88, 88, 88, 0.6)';
 
         for (var i = 1; i < 5; i++) {
             var size = i * (space) - this.padding;
-            this.draw.circle(size).move((this.size / 2) - (size / 2), (this.size / 2) - (size / 2)).fill('transparent').stroke('#585858');
+            this.draw.circle(size).move((this.size / 2) - (size / 2), (this.size / 2) - (size / 2)).fill('transparent').stroke(bgColor);
             this.draw
                 .text("Zone " + i)
-                .move((this.size / 2) - 14, (this.size / 2) + (size / 2) - 40)
-                .rotate(-90)
-                .font({ size: 12 });
+                .addClass('bg-label')
+                .center((this.size / 2), (this.size / 2) + (size / 2) - 20)
+                .rotate(-90);
         }
 
         var halfSize = (this.size / 2);
-        this.draw.line(0, halfSize, this.size, halfSize).stroke({width: 1})
-        this.draw.line(halfSize, 0, halfSize, this.size).stroke({width: 1})
+        this.draw.line(0, halfSize, this.size, halfSize).stroke({width: 1, color: bgColor})
+        this.draw.line(halfSize, 0, halfSize, this.size).stroke({width: 1, color: bgColor})
     };
 
     this.createLegends = function (quadrant, items) {
@@ -117,50 +118,24 @@ function Radar() {
                 url: url,
                 coords: {r: 0, t: 0},
                 movement: "c",
-                color: event.target.style.background
-            });
-            quadrant.items.addChild(newItem, function (err, res) {
-                if (!err) {
-                    // res is the saved child
-                }
+                color: quadrant.color
             });
 
-            this.drawItem(newItem);
+            quadrant.items.addChild(newItem, function (err, res) {
+                if (!err) {
+                    this.render();
+                }
+            }.bind(this));
 
         }.bind(this);
 
         return button;
     };
 
-    // { name: "Pair Programmincoords, coords: { r: 130, t: 170 }, movement: "c"}
+    // { name: "Pair Programming", coords: { r: 130, t: 170 }, movement: "c"}
     this.drawItem = function (item) {
 
-        var group = this.draw.group().draggable();
-        /*
-        group.draggable(function(x, y) {
-
-        var coords = {x: true, y: true};
-        var itemSize = 24,
-            offset = (this.size / 2) - (itemSize / 2);
-
-        if (x + offset < this.padding) {
-            coords.x = this.padding - offset;
-        }
-        else if (x + offset > this.size - this.padding) {
-            coords.x = this.size - this.padding - offset;
-        }
-
-        if (y + offset < this.padding) {
-            coords.y = this.padding - offset;
-        }
-        else if (y + offset > this.size - this.padding) {
-            coords.y = this.size - this.padding - offset;
-        }
-
-        return coords;
-
-        }.bind(this));
-        */
+        var group = this.draw.group().draggable().addClass('item');
 
         var itemSize = 24;
         var point = this.polarToCartesian(item.coords.r, item.coords.t);
@@ -174,9 +149,10 @@ function Radar() {
         title.textContent = " " + item.index + " ";
         group.node.appendChild(title);
         group.circle(itemSize, itemSize).center(x, y).attr({fill: item.color});
-        group.text(item.index).center(x, y + 4).font({ size: 12 });
+        group.text(item.index).center(x, y);
         group.node.style.cursor = "pointer";
 
+        group.data('item-name', item.name);
         group.data('item-id', item._id);
 
         group.off('dragend').on('dragend', function(event){
@@ -216,26 +192,32 @@ function Radar() {
         group.off('contextmenu').on('contextmenu', function(event) {
             event.preventDefault();
 
-            if (confirm("Soll das Element \""+ event.currentTarget.textContent +"\" endgültig gelöscht werden?")) {
-                event.currentTarget.remove();
-
-                var item = undefined;
-                this.db.items.getById(event.currentTarget.getAttribute('data-item-id'), function(err, res){
-                    if (!err){
-                        item = res;
-                    }
-                });
-
-                item.delete(function(err, res){
-                    if (!err){
-                        // res is the ID of the deleted object
-                        console.log('The item "'+ res.result.join() +'" is deleted!');
-                    }
-                });
-            }
+            this.deleteItem(event.currentTarget);
 
             return false;
         }.bind(this));
+    };
+
+    this.deleteItem = function(element) {
+        var name = element.getAttribute('data-item-name');
+        if (confirm("Soll das Element \"" + name + "\" endgültig gelöscht werden?")) {
+            element.remove();
+
+            var item = undefined;
+            this.db.items.getById(element.getAttribute('data-item-id'), function (err, res) {
+                if (!err) {
+                    item = res;
+                }
+            });
+
+            item.delete(function (err, res) {
+                if (!err) {
+                    // res is the ID of the deleted object
+                    console.log('Item "' + res.result.join() + '" deleted!');
+                    this.render();
+                }
+            }.bind(this));
+        }
     };
 
     this.render = function () {
